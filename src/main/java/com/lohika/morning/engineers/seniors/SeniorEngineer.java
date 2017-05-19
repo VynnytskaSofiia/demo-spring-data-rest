718b8eb7816cd03d37d9a95c1f3268cbc6e6ba7f
SofiaVynnytska <sonkavyn@gmail.com>
2017-05-19 21:53:47 +0300

    Seniors

--- src/main/java/com/lohika/morning/engineers/seniors/SeniorEngineer.java
+++ src/main/java/com/lohika/morning/engineers/seniors/SeniorEngineer.java
@@ -5,6 +5,6 @@ import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
-@Document(collection = "juniors")
+@Document(collection = "seniors")
 public class SeniorEngineer extends JuniorEngineer {
 }
