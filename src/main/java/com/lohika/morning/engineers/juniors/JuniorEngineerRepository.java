dcec5bc64f2ad40cfbe8efd69e94254394544eb5
VynnytskaSofiia <svinnytska@n-ix.com>
2017-05-19 11:47:10 +0300

    Added search and Pagination

--- src/main/java/com/lohika/morning/engineers/juniors/JuniorEngineerRepository.java
+++ src/main/java/com/lohika/morning/engineers/juniors/JuniorEngineerRepository.java
@@ -1,11 +1,19 @@
 package com.lohika.morning.engineers.juniors;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
 
 @RepositoryRestResource(path = "juniors")
-interface JuniorEngineerRepository extends CrudRepository<JuniorEngineer, String> {
+interface JuniorEngineerRepository extends PagingAndSortingRepository<JuniorEngineer, String> {
 
+    @Override
+    @RestResource(exported = false)
+    void delete(String Id);
+
+    List<JuniorEngineer> findByProjectIsNull();
 }
 
 
