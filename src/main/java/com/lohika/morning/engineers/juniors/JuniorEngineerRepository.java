6b804ba99cad65e6904e595381bb1753ec2d6769
VynnytskaSofiia <svinnytska@n-ix.com>
2017-05-19 11:14:15 +0300

    Juniors API spring-data-rest style

--- src/main/java/com/lohika/morning/engineers/juniors/JuniorEngineerRepository.java
+++ src/main/java/com/lohika/morning/engineers/juniors/JuniorEngineerRepository.java
@@ -1,7 +1,9 @@
 package com.lohika.morning.engineers.juniors;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "juniors")
 interface JuniorEngineerRepository extends CrudRepository<JuniorEngineer, String> {
 
 }
